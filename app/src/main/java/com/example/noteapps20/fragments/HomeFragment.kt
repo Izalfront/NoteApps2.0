6095c082e2dc49b7f4e7e8a02b7afecfa02d3206
izalfront <rizalhajakubastarus@gmail.com>
2024-04-05 11:29:33 +0800

    develop 90%

--- app/src/main/java/com/example/noteapps20/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/noteapps20/fragments/HomeFragment.kt
@@ -47,16 +47,13 @@ class HomeFragment : Fragment(R.layout.fragment_home), SearchView.OnQueryTextLis
         }
     }
 
-    private fun updateUI(note: List<Note>?){
-        if(note != null){
-            if(note.isEmpty())
-            {
-                binding.emptyNotesImage.visibility = View.GONE
-                binding.homeRecyclerView.visibility = View.VISIBLE
-            } else{
-                binding.emptyNotesImage.visibility = View.VISIBLE
-                binding.homeRecyclerView.visibility = View.GONE
-            }
+    private fun updateUI(note: List<Note>?) {
+        if (note.isNullOrEmpty()) {
+            binding.emptyNotesImage.visibility = View.VISIBLE
+            binding.homeRecyclerView.visibility = View.GONE
+        } else {
+            binding.emptyNotesImage.visibility = View.GONE
+            binding.homeRecyclerView.visibility = View.VISIBLE
         }
     }
 
